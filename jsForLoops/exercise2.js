848fe6f60aeb06e7ac28d857a26b064a4884c4de
DanielleIzawa <izawa.danielle@gmail.com>
2022-09-19 19:22:35 -0300

    js break loops, for loops and while loops exercises

---
+++ jsForLoops/exercise2.js
@@ -0,0 +1,6 @@
+// Create a loop that runs from 0 to 9.
+
+const fruits = ["Apple", "Banana", "Orange"];
+for (x of fruits) {
+  console.log(x);
+}
---
+++ jsWhileLoops/exercise1.js
@@ -0,0 +1,7 @@
+// Create a loop that runs as long as i is less than 10.
+
+let i = 0
+while (1 < 10) {
+  console.log(i);
+  i++
+}
